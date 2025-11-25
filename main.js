dbd385e40764e2552a2979772398c0df199dd879
Nick Vlahogiannis <526Nvlahogiannis@frhsd.com>
2025-11-25 11:27:48 -0500

    5th name

--- main.js
+++ main.js
@@ -4,8 +4,15 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       image: "./assets/images/socks_blue.jpg",
-      inventory: 100,
-      onSale: true,
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      // solution
+      sizes: ["S", "M", "L", "XL"],
+      // solution
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
     };
   },
 });
